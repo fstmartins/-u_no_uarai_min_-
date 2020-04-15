dd7aead5960b6eb663edc01163be71d607c2e6c2
Jaime Reis <jaime_reis18@outlook.pt>
2020-04-15 17:27:17 +0100

    Database structure and connection done, fetching all users

--- rapBattle/src/main/java/org/charlie/rapbattle/model/User.java
+++ rapBattle/src/main/java/org/charlie/rapbattle/model/User.java
@@ -1,4 +1,65 @@
 package org.charlie.rapbattle.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int battlesWon;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getBattlesWon() {
+        return battlesWon;
+    }
+
+    public void setBattlesWon(int battlesWon) {
+        this.battlesWon = battlesWon;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
