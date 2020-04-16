5f7a85df8d0e6f4a3beb5b1ae4ab8ad16e0fbc80
Fábio Martins - Trabalho <Trabalho_FSM@MBP-de-Fabio.Home>
2020-04-16 18:30:53 +0000

    Did the connection of the main controller to the first room

--- rapBattle/src/main/java/org/charlie/rapbattle/controllers/MainController.java
+++ rapBattle/src/main/java/org/charlie/rapbattle/controllers/MainController.java
@@ -1,6 +1,7 @@
 package org.charlie.rapbattle.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,8 +15,8 @@ public class MainController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value="room/{id}")
-    public String room() {
-        return "room";
+    public String room(@PathVariable Integer id) {
+        return "room" + id;
     }
 }
 
