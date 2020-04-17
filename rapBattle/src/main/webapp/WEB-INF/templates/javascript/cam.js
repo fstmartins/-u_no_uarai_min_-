2ac2cc5f33e4a8b597ea2ea8927fd0604dea7182
Gustavo <gustavo.mendes@live.com.pt>
2020-04-17 02:21:11 +0000

    webcam working again, finished rooms and implemented home button

--- rapBattle/src/main/webapp/WEB-INF/templates/javascript/cam.js
+++
@@ -1,7 +0,0 @@
-var player = document.getElementById("video1")
-var handleSuccess = function(stream) {
-    video1.srcObject = stream;
-    console.log("success");
-};
-navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then(handleSuccess);
